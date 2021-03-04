a271c776c7d7c6f9a8a36815f08b512625b6fd57
jhakulinen <hakulisenjukka96@gmail.com>
2021-03-04 23:28:38 +0200

    Valmis, ehkä?

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 const PORT = process.env.PORT || 3000;
 
-
+// Luodaan reitti "etusivulle". //
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 
+// Luodaan reitti, joka hakee JSON-tiedoston ja parsii sen taulukkoon. //
+// Tein tämän kohdan alusta asti hieman väärin, joten visuaalinen muokkaaminen Bootstrapin avulla jäi pois, koska en tajunnut tehdä html-sivua, johon taulukko tulostuu. //
 app.get("/guestbook", function (req, res) {
     var json = require(__dirname + "/public/guestbook.json");
 
@@ -28,18 +30,18 @@ app.get("/guestbook", function (req, res) {
     }
     res.send(results);
 });
-
+// Luodaa neitti "/newmessage" sivulle, jossa voi lähettää uuden viestin vieraskirjaan. // 
 app.get("/newmessage", function (req, res) {
     res.sendFile(__dirname + "/public/newmessage.html");
 });
-
+// Tämä reitti hoitaa "/newmessage" sivun datan lähettämisen JSON-muodossa serverille, jossa se tallennetaan JSON-muotoon. // 
 app.post("/adduser", function (req, res) {
     var data = require("./public/guestbook.json");
-
+    // Tehdään if/else -lause, joka tarkistaa onko tyhjiä kenttiä. Jos yksikin kentistä on tyhjä, niin sivu latautuu uudelleen. //
     if (req.body.username == "" || req.body.country == "" || req.body.message == "") {
-        res.redirect("http://localhost:3000/newmessage")
+        res.redirect("/newmessage")
     } else {
-
+        // Lähetetään lomakkeed tiedot JSON-muodossa serverille ja tallennetaan ne guestbook.json tiedostoon. //
         data.push({
             "username": req.body.username,
             "country": req.body.country,
@@ -53,14 +55,16 @@ app.post("/adduser", function (req, res) {
             if (err) throw err;
             console.log("Data saved!")
         });
-        res.redirect("http://localhost:3000/guestbook")
+        // Kun kaikki on valmista, ohjataan käyttäjä "/guestbook" sivulle. //
+        res.redirect("/guestbook")
     }
 });
-
+// Luodaan reitti "/ajaxmessage", joka lataa saman nimisen html-sivun. //
 app.get("/ajaxmessage", function (req, res) {
     res.sendFile(__dirname + "/public/ajaxmessage.html");
 });
 
+// Käytännössä sama toimenpide, kuin "/newmessage" sivulla, mutta ilman uudelleenohjausta, koska "/ajaxmessage" tulostaa taulukon näkyviin samalle sivulle. //
 app.post("/ajaxmessage", function (req, res) {
     var data = require("./public/guestbook.json");
 
@@ -70,8 +74,7 @@ app.post("/ajaxmessage", function (req, res) {
         "date": new Date(),
         "message": req.body.message
     });
-    console.log(data)
-    console.log(req.body)
+
 
     var jsonStr = JSON.stringify(data);
 
@@ -86,6 +89,7 @@ app.get("*", function (req, res) {
     res.send("Can't find the requested page", 404)
 });
 
+// Käsketään ohjelma kuuntelemaan, joko Herokun määrittämää porttia tai porttia 3000. // 
 app.listen(PORT, function () {
     console.log("Server is running!")
 })
